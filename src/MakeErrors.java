4a82be4a348cb26e7618fbdec7ecf9e65858a5a0
rae <raej0000@gmail.com>
2025-01-21 11:15:21 -0800

    lab1

---
+++ src/MakeErrors.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class MakeErrors {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Please type the filepath (name included) relative to this file, of the file you would like to create errors in:");
+        String filepath = userInput.nextLine();
+
+    }
+}
