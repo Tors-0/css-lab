3dadaae40d7a5982cf140893ecb98868d31a3859
rae <raej0000@gmail.com>
2025-02-01 15:54:43 -0800

    lab 4

---
+++ src/Lab4B.java
@@ -0,0 +1,49 @@
+// Lab4B.java
+// CSSSKL 142
+// <Your Name>
+
+import java.util.Scanner;
+import java.lang.*;
+
+public class Lab4B {
+    public static void main(String[] args) {
+
+        // Call all methods here
+
+    }
+
+	// straightLineBox
+	// Produces and displays a box of straight lines using asterisks
+	// size: the length of the sides of the box
+    public static void straightLineBox(int size) {
+
+        // TODO
+
+    }
+
+	// boxMaker
+	// Ask the user for an integer size, and print the box using asterisks
+    public static void boxMaker() {
+
+        // TODO
+
+    }
+
+	// triangle
+	// Print a shape that is a triangular pattern with sides of 6 asterisks
+	// number: the width and length of the triangle
+    public static void triangle(int number) {
+
+       // TODO
+
+    }
+
+	// xPattern
+	// Print a shape that is an "X" pattern with arms of 3 asterisks each
+	// number: the length of the arms of the "X"
+    public static void xPattern(int number) {
+
+       // TODO
+
+    }
+}
