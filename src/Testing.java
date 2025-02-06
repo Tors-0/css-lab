089f0a6a12d9aebbdbf88568354c3030ef362f6b
rae <raej0000@gmail.com>
2025-02-06 13:00:41 -0800

    ica file i/o

--- src/Testing.java
+++ src/Testing.java
@@ -1,3 +1,4 @@
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Testing {
@@ -7,47 +8,11 @@ public class Testing {
 //        System.out.println(add(a, b)); // == 1523944
 //        System.out.println(a + b); // == 1523944
 
-        // ICA Loops 3
-        float totalFare = 0;
-        int totalCustomers = 0;
-        int totalRides = 0;
-
-        int currentDriver = 1;
-
-        Scanner driverConsole = new Scanner(System.in);
-
-        boolean driversYetToBeServed = true;
-        while (driversYetToBeServed) {
-            System.out.println("\nNow serving Driver " + currentDriver);
-            do {
-                System.out.print("Enter fare amount for a ride: ");
-                float fare = driverConsole.nextFloat();
-                driverConsole.nextLine();
-
-                System.out.print("Enter number customers for a ride: ");
-                int customers = driverConsole.nextInt();
-                driverConsole.nextLine();
-
-                totalRides++;
-                totalFare += Math.abs(fare);
-                totalCustomers += Math.abs(customers);
-
-                if (customers < 0) {
-                    driversYetToBeServed = false;
-                }
-                if (fare < 0) {
-                    break;
-                }
-                System.out.println();
-            } while (true);
-            currentDriver++;
-        }
-
-        System.out.println("\nTotal rides: " + totalRides);
-        System.out.println("Total gross receipts: $" + totalFare);
-        System.out.println("Average fare for a ride: $" +
-                (totalFare / totalRides));
-        System.out.println("Total number customers: " + totalCustomers);
+        System.out.println(System.out);
+        PrintStream out = System.out;
+        System.out.println(System.out);
+        System.out.println(out);
+        out.println("hi");
     }
     public static int add(int a, int b) {
         byte bitSize = 31;
