746a3fd2ea5ecd4bcc526cd7a48b05180fb203ed
rae <raej0000@gmail.com>
2025-02-20 00:09:09 -0800

    lab 6 submission

--- src/Lab6_Diary.java
+++ src/Lab6_Diary.java
@@ -1,4 +1,6 @@
 // Necessary imports for file i/o
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.Scanner;             // Reads from stream
 import java.io.IOException;           // This exception must be caught and handled when dealing with streams         
 import java.io.PrintWriter;           // Writes to the stream
@@ -28,12 +30,28 @@ public class Lab6_Diary {
         mm = keyboard.nextInt();
         dd = keyboard.nextInt();
         yyyy = keyboard.nextInt();
+        keyboard.nextLine();
       
         System.out.println("Enter as many lines of prose you wish (for your to-do list or diary entry)");
       
         // START YOUR CODE HERE
-      
-        
+        try {
+            File outputFile = new File("diaryLog.txt");
+            outputFile.createNewFile();
+            output = new PrintWriter(new FileOutputStream(outputFile, true));
+        } catch (Exception e) {
+            System.out.println("Something went wrong while writing files...");
+            e.printStackTrace();
+            System.exit(0);
+        }
+
+        output.printf("Date: %2d/%2d/%4d %n", mm, dd, yyyy);
+        do {
+            prose = keyboard.nextLine();
+            output.println(prose);
+        } while (!"".equals(prose));
+
+        output.close();
     }
 }
 
