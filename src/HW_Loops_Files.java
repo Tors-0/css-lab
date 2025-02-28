48f2e13f4c51749da1277e51f2ae5527f250d826
rae <raej0000@gmail.com>
2025-02-28 13:12:02 -0800

    loop file hw

---
+++ src/HW_Loops_Files.java
@@ -0,0 +1,55 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+// Authors: Rae
+
+public class HW_Loops_Files {
+    static double programWeight;
+    static double midtermWeight;
+    static double finalWeight;
+
+    public static void main(String[] args) {
+        int courseNumber;            // Number of the course
+        Scanner inputFile = null;    // File containing data (p. 297
+        //     in Savitch discusses null)
+
+        // ... code assigning inputFile in a try/catch statement
+        try {
+            inputFile = new Scanner(new FileInputStream("courseData.txt"));
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred: " + e.getMessage());
+            e.printStackTrace(System.out);
+        }
+
+        // ... code for any stuff you need to do one time ...
+        programWeight = inputFile.nextInt();
+        midtermWeight = inputFile.nextInt();
+        finalWeight = inputFile.nextInt();
+
+        //Per class, print a table of ID numbers, grades, weighted average
+        //    per student, and a Pass or Fail programs grade.  The class
+        //    average is also printed.
+        for (;true;)
+        {
+            // Read class number, print class number title, headings.
+            courseNumber = inputFile.nextInt();
+
+
+            // initialization
+
+
+            // Loop to handle one class:
+            //     For each student in the class, get and print their
+            //     information, compute their avg, and sum the avgs.
+            while (true)
+            {
+                break;
+            }
+
+            // compute and print class average
+            break;
+        }
+    }
+}
