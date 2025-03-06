50415ddfa2d3a95eebba55ed1b6ead1cebc58ac4
rae <raej0000@gmail.com>
2025-03-06 12:31:52 -0800

    classwork classes 2 cont.

--- src/ica/Walker.java
+++ src/ica/Walker.java
@@ -9,10 +9,10 @@ public class Walker {
     private int dx = 0;
     private int dy = 0;
 
-    public final int limitX = 20;
+    public final int limitX = 80;
     public final int limitY = 20;
 
-    public final int wait;
+    private int wait;
 
     public Walker(int initX, int initY, int wait) {
         x = initX;
@@ -20,17 +20,47 @@ public class Walker {
         this.wait = wait;
     }
 
+    public void queueX(int num) {
+        dx += num;
+    }
+    public void queueY(int num) {
+        dy -= num;
+    }
     public void moveUp(int dist) {
-        dy += dist;
+        dy -= dist;
+        while (dy != 0) {
+            this.waitMillis(this.wait);
+            this.tick();
+            clearScreen();
+            this.drawWalker();
+        }
     }
     public void moveDown(int dist) {
-        dy -= dist;
+        dy += dist;
+        while (dy != 0) {
+            this.waitMillis(this.wait);
+            this.tick();
+            clearScreen();
+            this.drawWalker();
+        }
     }
     public void moveLeft(int dist) {
         dx -= dist;
+        while (dx != 0) {
+            this.waitMillis(this.wait);
+            this.tick();
+            clearScreen();
+            this.drawWalker();
+        }
     }
     public void moveRight(int dist) {
         dx += dist;
+        while (dx != 0) {
+            this.waitMillis(this.wait);
+            this.tick();
+            clearScreen();
+            this.drawWalker();
+        }
     }
     public Vector2i getCurrentPos() {
         return new Vector2i((this.x + dx) % limitX, (this.y + dy) % limitY);
@@ -39,8 +69,28 @@ public class Walker {
         return new Vector2i(this.x, this.y);
     }
     public void tick() {
-        this.x = (this.x + dx) % limitX;
-        this.y = (this.y + dy) % limitY;
+        // move the walker one space in the specified directions
+        if (dx != 0)
+            this.x = (this.x + dx / Math.abs(dx));
+        if (dy != 0)
+            this.y = (this.y + dy / Math.abs(dy));
+
+        if (x > limitX) {
+            x %= limitX;
+        } else if (x < 0) {
+            x += limitX;
+        }
+        if (y > limitY) {
+            y %= limitY;
+        } else if (y < 0) {
+            y += limitY;
+        }
+
+        // remove one from each direction
+        if (dx > 0) dx--;
+        else if (dx < 0) dx++;
+        if (dy > 0) dy--;
+        else if (dy < 0) dy++;
     }
     public void waitMillis(int numMilliseconds) {
         try {
@@ -52,11 +102,23 @@ public class Walker {
     }
     public void drawWalker() {
         for (int i = 0; i < this.y; i++) {
-            System.out.println();
+            System.out.print("\n");
         }
         for (int i = 0; i < this.x; i++) {
             System.out.print(' ');
         }
         System.out.print(this.walker);
+    }
+
+    static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    public int getWait() {
+        return wait;
+    }
+    public void setWait(int wait) {
+        this.wait = wait;
     }
 }
--- src/ica/WalkerDriver.java
+++ src/ica/WalkerDriver.java
@@ -1,18 +1,33 @@
 package ica;
 
+import static ica.Walker.clearScreen;
+
 public class WalkerDriver {
     public static void main(String[] args) {
-        Walker myWalker = new Walker(5, 5, 50);
+        Walker myWalker = new Walker(0, 5, 250);
 
-        testDrawing(myWalker);
+        myWalker.moveRight(10);
+
+        myWalker.setWait(15); // by the magic of the druids, i increase my speed
+
+        myWalker.moveRight(10);
+        myWalker.moveDown(5);
+        myWalker.moveLeft(10);
+        myWalker.moveUp(5);
+
+        myWalker.setWait(25);
+
+        myWalker.moveUp(30);
+        myWalker.queueY(-350);
+        myWalker.moveRight(350);
     }
     public static void testDrawing(Walker walker) {
 
-        walker.waitMillis(walker.wait); // wait some time
-        System.out.print('\u000C'); // clear screen
+        walker.waitMillis(walker.getWait()); // wait some time
 
-        walker.tick();
-        walker.drawWalker();
+        walker.tick(); // update walker position
 
+        clearScreen(); // clear screen
+        walker.drawWalker(); // draw new position
     }
 }
